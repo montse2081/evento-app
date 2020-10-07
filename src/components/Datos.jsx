e7d50b1d8e79e34fe140f89d1ffd183de052f319
montse2081 <52506559+montse2081@users.noreply.github.com>
2020-10-06 21:50:24 -0500

    version 1

---
+++ src/components/Datos.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+
+function Datos(props) {
+    return (
+    <>
+    <div>Name : {props.name}</div>
+    <div>Apellido: {props.lastname}</div>
+      </>  );
+  }
+  export default Datos;
---
+++ src/components/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Form() {
+    return (
+        <>
+        <div>
+            <figure class="image is-228x228">
+            <img class="is-rounded" src="https://www.fiestayboda.com/wp-content/uploads/2016/11/la-figura-de-la-wedding-planner-a-debate-en-fiesta-y-boda.jpg"/>
+            </figure>
+
+        </div>
+      
+          </>  );
+  }
+  export default Form;
